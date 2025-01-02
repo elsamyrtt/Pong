0d25ba7fdca28d793657c23597168154bcf88d37
elsamyrtt <salegandro895@gmail.com>
2025-01-02 01:58:26 -0500

    Esto necesita un parche

--- pong/src/main/java/com/pong/Panel.java
+++ pong/src/main/java/com/pong/Panel.java
@@ -4,28 +4,66 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.concurrent.ThreadLocalRandom;
 
 import javax.swing.JPanel;
 
 public class Panel extends JPanel{
     //Player Left && Player Right
-    Player PL = new Player();
-    Player PR = new Player(900 - 35,350);
+    public Player PL = new Player();
+    public Player PR = new Player(900 - 35,350);
+    int SPEED = 20;
 
-    public Panel(){
-        setBackground(Color.decode("#292929"));
+    private int getRandomNumber(int min, int max){
+       return ThreadLocalRandom.current().nextInt(min, max);
     }
 
+
+    //Entradas de teclado
+    public Panel(){
+        addKeyListener(new KeyListener() {
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode() == KeyEvent.VK_W){
+                    PL.y -= SPEED;
+                    repaint();
+                }
+                if(e.getKeyCode() == KeyEvent.VK_S){
+                    PL.y += SPEED;
+                    repaint();
+                }
+                if(e.getKeyCode() == KeyEvent.VK_UP){
+                    PR.y -= SPEED;
+                    repaint();
+                }
+                if(e.getKeyCode() == KeyEvent.VK_DOWN){
+                    PR.y += SPEED;
+                    repaint();
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+            }
+
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+            
+        });
+        this.setFocusable(true);
+    }
+    
     //dibujando graficos
     @Override
     public void paint(Graphics g) {
         super.paint(g);
-    }
-
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
+        g.setColor(new Color(getRandomNumber(1, 251)+3, getRandomNumber(1, 250)+4, getRandomNumber(1, 249)+5));
         g.fillRect(PL.x, PL.y, PL.WIDTH, PL.HEIGHT);
+        g.setColor(new Color(getRandomNumber(1, 251)+3, getRandomNumber(1, 250)+4, getRandomNumber(1, 249)+5));
         g.fillRect(PR.x, PR.y, PR.WIDTH, PR.HEIGHT);
     }
 
